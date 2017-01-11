69e48acf4a39a298713ec1eef331b1553c31ff43
ahmedkrajcin <ahmedkrajcin@gmail.com>
2017-01-11 23:23:16 +0100

    Advetisment conected to firebase and Schedule fragment is working

--- app/src/main/java/com/example/samir/navigationtest/Fragments/AdvertismentFragment.java
+++ app/src/main/java/com/example/samir/navigationtest/Fragments/AdvertismentFragment.java
@@ -6,15 +6,22 @@ import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.example.samir.navigationtest.Adapters.AdAdapter;
 import com.example.samir.navigationtest.Modules.Ad;
 import com.example.samir.navigationtest.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 // Fragment used to show Advertisment
 public class AdvertismentFragment extends PlaceholderFragment {
 
-    ArrayList<Ad> ads;
+    ArrayList<Ad> ads=new ArrayList<Ad>();
     StaggeredGridLayoutManager mStaggeredLayoutManager;
     RecyclerView mRecyclerView;
     AdAdapter mAdAdapter;
@@ -23,9 +30,39 @@ public class AdvertismentFragment extends PlaceholderFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_ads, container, false);
-        ArrayList<Ad> ads = getPlaces();
-        mAdAdapter = new AdAdapter(ads);
+        //initializePlaces();
+      // ArrayList<Ad> ads;
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = database.getReference();
+        databaseReference.child("Advertisement").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+                for (DataSnapshot child : children) {
+                    Ad r = child.getValue(Ad.class);
+                    boolean cond = true;
+                    for (Ad i:ads) {
+                        if(i == r){
+                            cond = false;
+                        }
+                    }
+                    if(cond){
+                        ads.add(r);
+                    }
+                    else
+                        cond = true;
+                    mAdAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        mAdAdapter = new AdAdapter(ads);
         mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
         mStaggeredLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(mStaggeredLayoutManager);
@@ -35,7 +72,7 @@ public class AdvertismentFragment extends PlaceholderFragment {
         return rootView;
     }
 
-    public ArrayList<Ad> getPlaces() {
+    public void initializePlaces() {
         ArrayList<Ad> places = new ArrayList<>();
         // Creating ads in database should be considered
         places.add(new Ad("http://www.akta.ba/resources/Article/SmallImages/885b461e-5456-414f-9ae9-dd133bc0a3datoskana_mala.jpg", "Biss-Tours"));
@@ -44,7 +81,13 @@ public class AdvertismentFragment extends PlaceholderFragment {
         places.add(new Ad("http://072info.com/wp-content/uploads/2014/04/BRATISLAVA-BEC-BUDIMPESTA-BISS-TOURS.jpg", "Biss-Tours"));
         places.add(new Ad("http://www.fksarajevo.ba/assets/uploads/articles/centrotrans-eurolines-se-ukljucuje-u-lbc-projekat-fk-sarajevo.jpg", "Centrotrans"));
         places.add(new Ad("http://www.akta.ba/resources/Article/SmallImages/91c97092-9326-4f48-8d65-6f53fbe27f34Spanija_Bisstours1.jpg", "Biss-Tours"));
-        return places;
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = database.getReference();
+        for (int i=0;i<places.size();i++) {
+            databaseReference.child("Advertisements").push().setValue(places.get(i));
+        }
+
+
     }
 
 }
--- app/src/main/java/com/example/samir/navigationtest/Fragments/ScheduleFragment.java
+++ app/src/main/java/com/example/samir/navigationtest/Fragments/ScheduleFragment.java
@@ -1,41 +1,28 @@
 package com.example.samir.navigationtest.Fragments;
-
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SearchView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
 import android.widget.Toast;
-
 import com.example.samir.navigationtest.Adapters.ScheduleAdapter;
-import com.example.samir.navigationtest.MainActivity;
 import com.example.samir.navigationtest.Modules.SimpleRoute;
 import com.example.samir.navigationtest.R;
-import com.google.android.gms.drive.internal.QueryRequest;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
-
-
 /**
  * Created by Vildan on 16.11.2016..
  */
-
 // Fragment that shows the schedule info
 public class ScheduleFragment extends PlaceholderFragment {
-
-
     private StaggeredGridLayoutManager mStaggeredGridLayoutManager;
     private RecyclerView recyclerView;
     private ScheduleAdapter scheduleAdapter;
@@ -43,35 +30,29 @@ public class ScheduleFragment extends PlaceholderFragment {
     private com.toptoche.searchablespinnerlibrary.SearchableSpinner spinner2;
     private ArrayList<SimpleRoute> routes;
     private String selection;
-
-
+    private String selection1;
+    private String selection2;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         routes = new ArrayList<>();
-        selection = "Sarajevo";
+        selection = "Sarajevo-Visoko";
+        selection1 = "Sarajevo";
+        selection2 = "Visoko";
         //initializeRoutes();
-
-
+        //initializeRoutes2();
         View view = inflater.inflate(R.layout.fragment_schedule, container, false);
-
         spinner1 =  (com.toptoche.searchablespinnerlibrary.SearchableSpinner) view.findViewById(R.id.locationn);
         spinner2 = (com.toptoche.searchablespinnerlibrary.SearchableSpinner) view.findViewById(R.id.destinationn);
-
         Button displayRoutes = (Button) view.findViewById (R.id.btnDisplayRoutes);
-
         displayRoutes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 scheduleAdapter.scheduleList = getStation(selection);
                 scheduleAdapter.notifyDataSetChanged();
-
                 // Vildan stavio
                 Toast.makeText(view.getContext(), "You have searched!", Toast.LENGTH_LONG).show();
             }
         });
-
         final ArrayList<String> a = new ArrayList();
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = database.getReference();
@@ -84,8 +65,6 @@ public class ScheduleFragment extends PlaceholderFragment {
                     if(!a.contains(value))
                         a.add(value);
                 }
-
-
                 // Set 1 spinner
                 final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, a);
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -93,37 +72,35 @@ public class ScheduleFragment extends PlaceholderFragment {
                 spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                     @Override
                     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                        selection = a.get(position);
+                        selection1 = a.get(position);
+                        selection = selection1+"-"+selection2;
                     }
-
                     @Override
                     public void onNothingSelected(AdapterView<?> parent) {
-
                     }
                 });
-
                 //
-
                 // Set 2 spinner
                 ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, a);
                 adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 spinner2.setAdapter(adapter2);
+                spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        selection2 = a.get(position);
+                        selection = selection1+"-"+selection2;
+                    }
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+                    }
+                });
             }
-
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
             }
         });
-
         // when I get location and destination I need to find routes
-
-
-
-        //routes = getStation(selection);
-
-
+        //routes = getStation(selection1);
         // need routes MUST HAVE
         scheduleAdapter = new ScheduleAdapter(routes,getContext());
         recyclerView = (RecyclerView) view.findViewById(R.id.scheduleList);
@@ -132,18 +109,14 @@ public class ScheduleFragment extends PlaceholderFragment {
         recyclerView.setAdapter(scheduleAdapter);
         return view;
     }
-
-
-
     private ArrayList<SimpleRoute> getStation(String name) {
         final ArrayList<SimpleRoute> s = new ArrayList<>();
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = database.getReference();
+        // Main emphasis on the name as it allows the data to change when we change it
         databaseReference.child(name).addValueEventListener(new ValueEventListener() {
-
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-
                 Iterable<DataSnapshot> children = dataSnapshot.getChildren();
                 for (DataSnapshot child : children) {
                     SimpleRoute r = child.getValue(SimpleRoute.class);
@@ -158,17 +131,18 @@ public class ScheduleFragment extends PlaceholderFragment {
                     }
                     else
                         cond = true;
+                    if(!r.endAddress.equals(selection2)){
+                        // DO Something
+                    }
                     scheduleAdapter.notifyDataSetChanged();
                 }
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
             }
         });
         return s;
     }
-
     // works
     public void initializeRoutes(){
         ArrayList<String> VS = new ArrayList<>();
@@ -180,7 +154,6 @@ public class ScheduleFragment extends PlaceholderFragment {
         VS.add("Igmanska 44, Vogošća, Federacija Bosne i Hercegovine");
         VS.add("Maršala Tita 10, Sarajevo, Federacija Bosne i Hercegovine");
         VS.add("Autobuska stanica Sarajevo, Federacija Bosne i Hercegovine");
-
         ArrayList<String> SV = new ArrayList<>();
         SV.add("Autobuska stanica Sarajevo, Federacija Bosne i Hercegovine");
         SV.add("Hamze Hume, Sarajevo, Federacija Bosne i Hercegovine");
@@ -190,7 +163,6 @@ public class ScheduleFragment extends PlaceholderFragment {
         SV.add("Bosanski Put, Ilijas, Federacija Bosne i Hercegovine");
         SV.add("R445 Ljesevo, Federacija Bosne i Hercegovine");
         SV.add("Autobuska stanica Visoko, Federacija Bosne i Hercegovine");
-
         ArrayList<String> GS = new ArrayList<>();
         GS.add("Mašica Munje 8, Goražde, Federacija Bosne i Hercegovine");
         GS.add("Sarajevska, Goražde, Federacija Bosne i Hercegovine");
@@ -199,8 +171,6 @@ public class ScheduleFragment extends PlaceholderFragment {
         GS.add("Karađorđeva, Pale, Republika Srpska");
         GS.add("Bentbaša, Sarajevo, Federacija Bosne i Hercegovine");
         GS.add("Autobuska stanica Sarajevo, Federacija Bosne i Hercegovine");
-
-
         ArrayList<String> SG = new ArrayList<>();
         SG.add("Autobuska stanica Sarajevo, Federacija Bosne i Hercegovine");
         SG.add("Bentbaša, Sarajevo, Federacija Bosne i Hercegovine");
@@ -209,30 +179,71 @@ public class ScheduleFragment extends PlaceholderFragment {
         SG.add("R448, Jabuka, Republika Srpska");
         SG.add("Sarajevska, Goražde, Federacija Bosne i Hercegovine");
         SG.add("Mašica Munje 8, Goražde, Federacija Bosne i Hercegovine");
-
         ArrayList<SimpleRoute> routes1 = new ArrayList<>();
         routes1.add(new SimpleRoute("6:00","7:00","Visoko","Sarajevo",VS));
         routes1.add(new SimpleRoute("8:00","9:00","Visoko","Sarajevo",VS));
         routes1.add(new SimpleRoute("10:00","11:00","Visoko","Sarajevo",VS));
-
         routes1.add(new SimpleRoute("7:00","8:00","Sarajevo","Visoko",SV));
         routes1.add(new SimpleRoute("9:00","10:00","Sarajevo","Visoko",SV));
         routes1.add(new SimpleRoute("11:00","12:00","Sarajevo","Visoko",SV));
-
         routes1.add(new SimpleRoute("6:00","8:00","Sarajevo","Goražde",SG));
         routes1.add(new SimpleRoute("10:00","12:00","Sarajevo","Goražde",SG));
         routes1.add(new SimpleRoute("13:00","15:00","Sarajevo","Goražde",SG));
-
         routes1.add(new SimpleRoute("6:00","8:00","Goražde","Sarajevo",GS));
         routes1.add(new SimpleRoute("10:00","12:00","Goražde","Sarajevo",GS));
         routes1.add(new SimpleRoute("13:00","15:00","Goražde","Sarajevo",GS));
-
         // Sarajevo 43.858113, 18.412156
         // Visoko 43.987477, 18.177795
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = database.getReference();
         for (int i=0;i<routes1.size();i++) {
-            databaseReference.child(routes1.get(i).startAddress).push().setValue(routes1.get(i));
+            databaseReference.child(routes1.get(i).startAddress+"-"+routes1.get(i).endAddress).push().setValue(routes1.get(i));
+        }
+    }
+    public void initializeRoutes2(){
+        ArrayList<String> SZ = new ArrayList<>();
+        SZ.add("Autobuska stanica Sarajevo, Federacija Bosne i Hercegovine");
+        SZ.add("Autobuska stanica Visoko, Bosne Srebrene, Visoko, Federacija Bosne i Hercegovine");
+        SZ.add("Autobuska stanica Kakanj, Kakanj, Federacija Bosne i Hercegovine");
+        SZ.add("Autobuska stanica Zenica, Bulevar Kralja Tvrtka I, Zenica, Federacija Bosne i Hercegovine");
+        ArrayList<String> ZS = new ArrayList<>();
+        ZS.add("Autobuska stanica Sarajevo, Federacija Bosne i Hercegovine");
+        ZS.add("Autobuska stanica Visoko, Bosne Srebrene, Visoko, Federacija Bosne i Hercegovine");
+        ZS.add("Autobuska stanica Kakanj, Kakanj, Federacija Bosne i Hercegovine");
+        ZS.add("Autobuska stanica Zenica, Bulevar Kralja Tvrtka I, Zenica, Federacija Bosne i Hercegovine");
+        ArrayList<String> ST = new ArrayList<>();
+        ST.add("Autobuska stanica Sarajevo, Federacija Bosne i Hercegovine");
+        ST.add("M18, Semizovac, Federacija Bosne i Hercegovine");
+        ST.add("Autobuska stanica Olovo, Branilaca Olova, Olovo, Federacija Bosne i Hercegovine");
+        ST.add("M18, Kladanj, Federacija Bosne i Hercegovine");
+        ST.add("Autobuska stanica Živinice, Živinice Grad, Federacija Bosne i Hercegovine");
+        ST.add("Autobuska stanica Tuzla, Federacija Bosne i Hercegovine");
+        ArrayList<String> TS = new ArrayList<>();
+        TS.add("Autobuska stanica Tuzla, Federacija Bosne i Hercegovine");
+        TS.add("Autobuska stanica Živinice, Živinice Grad, Federacija Bosne i Hercegovine");
+        TS.add("M18, Kladanj, Federacija Bosne i Hercegovine");
+        TS.add("Autobuska stanica Olovo, Branilaca Olova, Olovo, Federacija Bosne i Hercegovine");
+        TS.add("M18, Semizovac, Federacija Bosne i Hercegovine");
+        TS.add("Autobuska stanica Sarajevo, Federacija Bosne i Hercegovine");
+        ArrayList<SimpleRoute> routes1 = new ArrayList<>();
+        //routes1.add(new SimpleRoute("6:00","7:00","Sarajevo","Zenica",SZ));
+        //routes1.add(new SimpleRoute("10:00","11:00","Sarajevo","Zenica",SZ));
+        //routes1.add(new SimpleRoute("13:00","14:00","Sarajevo","Zenica",SZ));
+        //routes1.add(new SimpleRoute("6:00","7:00","Zenica","Sarajevo",ZS));
+        //routes1.add(new SimpleRoute("10:00","11:00","Zenica","Sarajevo",ZS));
+        //routes1.add(new SimpleRoute("13:00","14:00","Zenica","Sarajevo",ZS));
+        routes1.add(new SimpleRoute("6:00","9:00","Tuzla","Sarajevo",TS));
+        routes1.add(new SimpleRoute("10:00","13:00","Tuzla","Sarajevo",TS));
+        routes1.add(new SimpleRoute("13:00","16:00","Tuzla","Sarajevo",TS));
+        routes1.add(new SimpleRoute("6:00","9:00","Sarajevo","Tuzla",ST));
+        routes1.add(new SimpleRoute("10:00","13:00","Sarajevo","Tuzla",ST));
+        routes1.add(new SimpleRoute("13:00","16:00","Sarajevo","Tuzla",ST));
+        // Sarajevo 43.858113, 18.412156
+        // Visoko 43.987477, 18.177795
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = database.getReference();
+        for (int i=0;i<routes1.size();i++) {
+            databaseReference.child(routes1.get(i).startAddress+"-"+routes1.get(i).endAddress).push().setValue(routes1.get(i));
         }
     }
 }
